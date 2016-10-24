78b0566e75a26ec5f3b2b728823841f47117f261
Brad Copenhaver and Tracie Weitzman <tracie@>
2016-10-24 10:21:16 -0700

    change Mad Libs to include looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,43 @@
 $(document).ready(function() {
-  $('#blanks form').submit(function(event) {
-    var firstnameInput = $("input#firstname").val();
-    var lastnameInput = $("input#lastname").val();
-    var addressInput = $("input#address").val();
-    var politicalInput = $("input#political").val();
 
-    $('.firstname').text(firstnameInput.toUpperCase());
-    $('.lastname').text(lastnameInput.toUpperCase());
-    $('.address').text(addressInput.toUpperCase());
-    $('.political').text(politicalInput.toUpperCase());
+  var elements = ["h1", "p", "img"];
+
+  elements.forEach(function(element) {
+    $(element).click(function() {
+      alert("This is a " + element + "!");
+    });
+  });
+  //
+  // $("h1").click(function() {
+  //   alert("This is a header.");
+  // });
+  //
+  // $("p").click(function() {
+  //   alert("This is a paragraph.");
+  // });
+  //
+  // $("img").click(function() {
+  //   alert("This is an image.");
+  // });
+
+  $('#blanks form').submit(function(event) {
+
+    var blanks = ["firstname", "lastname", "address", "political"]
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+
+      $('.' + blank).text(userInput.toUpperCase());
+    })
+    // var firstnameInput = $("input#firstname").val();
+    // var lastnameInput = $("input#lastname").val();
+    // var addressInput = $("input#address").val();
+    // var politicalInput = $("input#political").val();
+    //
+    // $('.firstname').text(firstnameInput.toUpperCase());
+    // $('.lastname').text(lastnameInput.toUpperCase());
+    // $('.address').text(addressInput.toUpperCase());
+    // $('.political').text(politicalInput.toUpperCase());
 
     $('#story').show();
 
